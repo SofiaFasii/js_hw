b24b7bca3acb53dcec646099a33b78933dd7fafb
Sofia <sofiafasii97@gmail.com>
2025-10-03 21:11:42 +0300

    o

--- js/homework7.js
+++ js/homework7.js
@@ -1,1 +1,10 @@
 console.clear();
+
+//1
+let mas = [9, 8 ,11];
+let p = document.querySelector(".numbers");
+p.textContent = `Було: ${mas}`;
+function checkTen(){
+    mas[1] = 10;
+    p.textContent = `Стало: ${mas}`;
+}
