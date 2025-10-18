f92bcaf3c5f3ca17b622a962854a7ceab16dc195
SofiaFasii <sofiafasii97@gmail.com>
2025-10-18 17:24:24 +0300

    перевірка

--- js/hw-9.js
+++ js/hw-9.js
@@ -1,1 +1,11 @@
 console.clear();
+
+//1
+let mas = ['HTML', 'CSS', 'JavaScript'];
+function logItems(mas){
+    for(let i = 0; i<mas.length; i++){
+        mas[i] 
+        console.log(`${i + 1} - ${mas[i]}`)
+    }
+}
+logItems(mas)
