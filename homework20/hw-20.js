f6f9bacb2804963caddc9a476420cd0b4d979b5b
SofiaFasii <sofiafasii97@gmail.com>
2025-12-30 19:27:04 +0200

    зробила дз 20

--- homework20/hw-20.js
+++ homework20/hw-20.js
@@ -1,3 +1,4 @@
+//1
 const galleryImages = document.querySelectorAll('.image');
 const fullImageContainer = document.querySelector('.full-image-container')
 const fullImage = document.querySelector('.full-image')
@@ -21,11 +22,11 @@ galleryImages.forEach((image, index) => {
 document.addEventListener('keydown', (event) => {
     if(fullImageContainer.style.display !== 'flex') return;
     
-    if(event.key === 'ArrowRight'){
+    if(event.key === 'ArrowRight' || event.key === 'd' || event.key === 'в'){
         currentImageIndex = (currentImageIndex + 1) % galleryImages.length;
         fullImage.src = galleryImages[currentImageIndex].src
     }
-    else if(event.key === 'ArrowLeft'){
+    else if(event.key === 'ArrowLeft' || event.key === 'a' || event.key === 'ф'){
         currentImageIndex = (currentImageIndex - 1 + galleryImages.length) % galleryImages.length;
         fullImage.src = galleryImages[currentImageIndex].src
     }
@@ -34,3 +35,41 @@ document.addEventListener('keydown', (event) => {
 })
 
 document.querySelector('.btn-closee').addEventListener('click', closeImage)
+//2
+const input = document.querySelector('#controls input');
+const renderBtn = document.querySelector('[data-action="render"]')
+const destroyBtn = document.querySelector('[data-action="destroy"]')
+const boxes = document.getElementById('boxes')
+
+function randomColor(){
+    return `rgb(${Math.random() * 256}, ${Math.random() * 256}, ${Math.random() * 256})`
+}
+function createBoxes(amount){
+    destroyBoxes()
+
+    let size = 30;
+
+    for(let i = 0; i < amount; i++){
+        const div = document.createElement('div')
+
+        div.style.width = `${size}px`;
+        div.style.height = `${size}px`;
+        div.style.margin = `5px`;
+        div.style.backgroundColor = randomColor();
+
+        size += 10
+        boxes.appendChild(div)
+    }
+}
+
+function destroyBoxes(){
+    boxes.innerHTML = ''
+}
+destroyBtn.addEventListener('click', destroyBoxes)
+
+renderBtn.addEventListener('click', () => {
+    const amount = input.value;
+    if(amount > 0){
+        createBoxes(amount)
+    }
+})
